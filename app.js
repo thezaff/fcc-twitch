3631b2d6d3f132e303594409b3585886c387a4e5
thezaff <zkhodjaev@wiut.uz>
2017-11-06 00:02:26 +0500

    except buttons every thing works

--- app.js
+++ app.js
@@ -12,13 +12,24 @@ $(document).ready(function () {
                 output += '<div class="row online"> <div class="col-xs-3 col-sm-2" id="logo"> <img class="img img-responsive img-circle" src="' + json.stream.channel.logo + '"></div>';
                 output += '<div class="col-xs-9 col-sm-3"><p class="stream online"><a href="https://www.twitch.tv/' + json.stream.channel.display_name + '">' + json.stream.channel.display_name + '</a></p></div>';
                 output += '<div class="col-xs-10 col-sm-7"><p class="game online"><b>' + json.stream.channel.game + '</b> : ' + json.stream.channel.status + '</p></div></div>';
-                $(output).appendTo("#outputBoard");                
-            } else {
-                offOutput += '<div class="row offline"><div class="col-xs-3 col-sm-2" id="logo"><i class="fa fa-minus-square" aria-hidden="true"></i></div>';
-                offOutput += '<div class="col-xs-9 col-sm-3"><p class="stream offline"><a href="https://www.twitch.tv/' + json.stream._links.channel + '">Offline</a></p></div>';
-                offOutput += '<div class="col-xs-10 col-sm-7"><p class="game offline"><b>' + 'No game' + '</b> : ' + '<i class="text-muted">Offline</i>' + '</p></div></div>'
+            } else if (json.stream == null) {
+                var tempArr = json._links.channel.split('/');
+                var name = tempArr[tempArr.length - 1]
+                output += '<div class="row offline"><div class="col-xs-3 col-sm-2" id="logo"><i class="fa fa-minus-square" aria-hidden="true"></i></div>';
+                output += '<div class="col-xs-9 col-sm-3"><p class="stream offline"><a href="https://www.twitch.tv/' + name + '">' + name + '</a></p></div>';
+                output += '<div class="col-xs-10 col-sm-7"><p class="game offline"><b>' + 'No game' + '</b> : ' + '<i class="text-muted">Offline</i>' + '</p></div></div>'
             }
-            $(offOutput).appendTo("#outputBoard");
-          });        
+            $("#outputBoard").html(output);                
+        });        
     }
+    $(".displayOns").click(function () {
+        $(".offline").css("display", "none");
+    });
+    $(".displayOffs").click(function () {
+        $(".online").css("display", "none");
+    });
+    $(".displayAll").click(function () {
+        $(".row").fadeIn(1000);
+        console.log("BBSE OK");
+    });
 });
